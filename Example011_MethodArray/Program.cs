684fc722e64b48c4725bcb390ce5c14be975ee8a
Jeiojey <signofaries@gmail.com>
2022-06-09 18:19:18 +0300

    Array&Method2 project

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 22, 31, 4, 56, 16, 4 , 71, 84 };
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
